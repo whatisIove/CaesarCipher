ef936a03cc5eb5c12b20a2fb08d673a700375c27
whatisIove <vladwfop@gmail.com>
2023-05-17 13:06:52 +0300

    v1.1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.datatransfer;
+    requires java.desktop;
+
+
     opens com.example.demo to javafx.fxml;
     exports com.example.demo;
 }
