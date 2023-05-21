46f5c06e05e59bc51590522120428b024eb489ff
whatisIove <vladwfop@gmail.com>
2023-05-21 20:02:28 +0300

    Gamma cipher v1.0

--- src/main/java/crypto/system/SymmetricEncryptionApp.java
+++ src/main/java/crypto/system/SymmetricEncryptionApp.java
@@ -2,7 +2,6 @@ package crypto.system;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
@@ -20,6 +19,7 @@ import java.awt.*;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Random;
 
 public class SymmetricEncryptionApp extends Application {
     private SymmetricEncryptionSystem encryptionSystem;
@@ -27,6 +27,7 @@ public class SymmetricEncryptionApp extends Application {
     private TextField keyTextField;
     private ComboBox<KeyType> keyTypeComboBox;
     private TextArea consoleTextArea;
+    private TextArea gammaTextArea;
 
     @Override
     public void start(Stage primaryStage) {
@@ -45,6 +46,7 @@ public class SymmetricEncryptionApp extends Application {
             consoleTextArea.clear();
             inputTextArea.clear();
             keyTextField.clear();
+            gammaTextArea.clear();
         });
 
         openMenuItem.setOnAction(event -> {
@@ -90,11 +92,9 @@ public class SymmetricEncryptionApp extends Application {
             }
         });
 
-
         exitMenuItem.setOnAction(event -> {
             primaryStage.close();
         });
-
 
         // About Menu
         Menu aboutMenu = new Menu("About");
@@ -102,7 +102,7 @@ public class SymmetricEncryptionApp extends Application {
         aboutMenu.getItems().add(aboutMenuItem);
 
         aboutMenuItem.setOnAction(event -> {
-            showAlert(Alert.AlertType.INFORMATION, "About",  "Developer of this project: Butok " +
+            showAlert(Alert.AlertType.INFORMATION, "About", "Developer of this project: Butok " +
                     "Vladislav 122m-22-3" + "\n" + "Email: butok.v.o@nmu.one");
         });
 
@@ -119,67 +119,121 @@ public class SymmetricEncryptionApp extends Application {
         Label keyLabel = new Label("Key:");
         keyTextField = new TextField();
 
-        // Key Type Area
+        // Key Type
         Label keyTypeLabel = new Label("Key Type:");
         keyTypeComboBox = new ComboBox<>();
-        keyTypeComboBox.getItems().addAll(KeyType.values());
-        keyTypeComboBox.getSelectionModel().selectFirst();
+        keyTypeComboBox.getItems().addAll(KeyType.DIMENSION_2, KeyType.DIMENSION_3, KeyType.NUMERIC, KeyType.TEXT);
+        keyTypeComboBox.setValue(KeyType.TEXT);
+
+        // Gamma Area
+        Label gammaLabel = new Label("Gamma:");
+        gammaTextArea = new TextArea();
+        gammaTextArea.setPrefHeight(100);
+        gammaTextArea.setEditable(false);
 
         // Encrypt Button
         Button encryptButton = new Button("Encrypt");
         encryptButton.setOnAction(event -> {
-            String input = inputTextArea.getText();
+            String inputText = inputTextArea.getText();
             String key = keyTextField.getText();
             KeyType keyType = keyTypeComboBox.getValue();
 
+            if (inputText.isEmpty() || key.isEmpty()) {
+                showAlert(Alert.AlertType.ERROR, "Error", "Input and key cannot be empty!");
+                return;
+            }
+
             try {
-                String encryptedText = encryptionSystem.encrypt(input, key, keyType);
-                consoleTextArea.appendText("Encryption successful\n");
-                consoleTextArea.appendText("Encrypted text: " + encryptedText + "\n");
-            } catch (Exception e) {
-                consoleTextArea.appendText("Encryption failed: " + e.getMessage() + "\n");
+                String gamma;
+                if (keyType == KeyType.DIMENSION_2) {
+                    String[] keyParts2D = key.split(",");
+                    if (keyParts2D.length != 2) {
+                        throw new IllegalArgumentException("Invalid key format for Dimension 2." + "\n" + "Expected format: coefficient1, coefficient2");
+                    }
+                    int coefficient1_2D = Integer.parseInt(keyParts2D[0]);
+                    int coefficient2_2D = Integer.parseInt(keyParts2D[1]);
+                    gamma = encryptionSystem.generateGamma(key, keyType, inputText);
+                } else if (keyType == KeyType.DIMENSION_3) {
+                    String[] keyParts3D = key.split(",");
+                    if (keyParts3D.length != 3) {
+                        throw new IllegalArgumentException("Invalid key format for Dimension 3." + "\n" + "Expected format: coefficient1, coefficient2, coefficient3");
+                    }
+                    int coefficient1_3D = Integer.parseInt(keyParts3D[0]);
+                    int coefficient2_3D = Integer.parseInt(keyParts3D[1]);
+                    int coefficient3_3D = Integer.parseInt(keyParts3D[2]);
+                    gamma = encryptionSystem.generateGamma(key, keyType, inputText);
+                } else if (keyType == KeyType.NUMERIC) {
+                    try {
+                        Integer.parseInt(key);
+                    } catch (NumberFormatException e) {
+                        throw new IllegalArgumentException("Invalid key format. Numeric key should be an integer.");
+                    }
+                    gamma = encryptionSystem.generateGamma(key, keyType, inputText);
+                } else {
+                    gamma = encryptionSystem.generateGamma(key, keyType, inputText);
+                }
+
+                String encryptedText = encryptionSystem.encrypt(inputText, gamma);
+
+                consoleTextArea.appendText("Encrypted:\n" + encryptedText + "\n");
+
+                gammaTextArea.setText(gamma);
+            } catch (IllegalArgumentException e) {
+                showAlert(Alert.AlertType.ERROR, "Error", e.getMessage());
             }
         });
 
-        // Decrypt Button
+// Decrypt Button
         Button decryptButton = new Button("Decrypt");
         decryptButton.setOnAction(event -> {
-            String input = inputTextArea.getText();
+            String inputText = inputTextArea.getText();
             String key = keyTextField.getText();
-            KeyType keyType = keyTypeComboBox.getValue();
+            String gamma = gammaTextArea.getText();
+
+            if (inputText.isEmpty() || key.isEmpty() || gamma.isEmpty()) {
+                showAlert(Alert.AlertType.ERROR, "Error", "Input, key, and gamma cannot be empty!");
+                return;
+            }
 
             try {
-                String decryptedText = encryptionSystem.decrypt(input, key, keyType);
-                consoleTextArea.appendText("Decryption successful\n");
-                consoleTextArea.appendText("Decrypted text: " + decryptedText + "\n");
-            } catch (Exception e) {
-                consoleTextArea.appendText("Decryption failed: " + e.getMessage() + "\n");
+                String decryptedText = encryptionSystem.decrypt(inputText, gamma);
+
+                consoleTextArea.appendText("Decrypted:\n" + decryptedText + "\n");
+            } catch (IllegalArgumentException e) {
+                showAlert(Alert.AlertType.ERROR, "Error", e.getMessage());
             }
         });
 
+
         // Console Area
+        Label consoleLabel = new Label("Console:");
         consoleTextArea = new TextArea();
+        consoleTextArea.setPrefHeight(100);
         consoleTextArea.setEditable(false);
 
         // Layout
         VBox root = new VBox();
-        root.setSpacing(10);
         root.setPadding(new Insets(10));
-        root.getChildren().addAll(menuBar, inputLabel, inputTextArea, keyLabel, keyTextField, keyTypeLabel, keyTypeComboBox,
-                encryptButton, decryptButton, consoleTextArea);
-        root.setAlignment(Pos.TOP_CENTER);
+        root.setSpacing(10);
+        root.getChildren().addAll(menuBar, inputLabel, inputTextArea, keyLabel, keyTextField, keyTypeLabel,
+                keyTypeComboBox, gammaLabel, gammaTextArea, encryptButton, decryptButton,
+                consoleLabel, consoleTextArea);
 
-        Scene scene = new Scene(root, 600, 600);
-        primaryStage.setTitle("Сryptosystem based on the Trithemius cipher");
+        // Scene
+        Scene scene = new Scene(root, 400, 600);
+
+        // Stage
+        primaryStage.setTitle("Cryptosystem based on the Gamble cipher");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    private void showAlert(Alert.AlertType alertType, String title, String content) {
+
+    private void showAlert(Alert.AlertType alertType, String title, String message) {
         Alert alert = new Alert(alertType);
         alert.setTitle(title);
         alert.setHeaderText(null);
-        alert.setContentText(content);
+        alert.setContentText(message);
         alert.showAndWait();
     }
 
@@ -196,139 +250,136 @@ public class SymmetricEncryptionApp extends Application {
     }
 }
 
+
 enum KeyType {
     DIMENSION_2,
     DIMENSION_3,
+    NUMERIC,
     TEXT
 }
 
 class SymmetricEncryptionSystem {
-    public boolean validateKey(String key, KeyType keyType) {
-        return switch (keyType) {
-            case DIMENSION_2 -> validateDimension2Key(key);
-            case DIMENSION_3 -> validateDimension3Key(key);
-            case TEXT -> validateTextKey(key);
-        };
 
-    }
 
-    private boolean validateDimension2Key(String key) {
-        // Перевірка валідності 2-вимірного вектору
-        return key.matches("[0-9]+,[0-9]+");
-    }
+    public String generateGamma(String key, KeyType keyType, String inputText) {
+        int gammaLength = inputText.length() + 1;
 
-    private boolean validateDimension3Key(String key) {
-        // Перевірка валідності 3-вимірного вектору
-        return key.matches("[0-9]+,[0-9]+,[0-9]+");
-    }
+        switch (keyType) {
+            case DIMENSION_2 -> {
+                String[] keyParts2D = key.split(",");
+                if (keyParts2D.length != 2) {
+                    throw new IllegalArgumentException("Invalid key format for Dimension 2. " +
+                            "Expected format: coefficient1,coefficient2");
+                }
 
-    private boolean validateTextKey(String key) {
-        // Перевірка валідності текстового ключа
-        return !key.isEmpty();
-    }
+                int coefficient1_2D;
+                int coefficient2_2D;
+                try {
+                    coefficient1_2D = Integer.parseInt(keyParts2D[0]);
+                    coefficient2_2D = Integer.parseInt(keyParts2D[1]);
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("Invalid key format. " +
+                            "Coefficients for Dimension 2 should be integers.");
+                }
 
-    public String encrypt(String input, String key, KeyType keyType) {
-        return switch (keyType) {
-            case DIMENSION_2 -> encryptWithDimension2Key(input, key);
-            case DIMENSION_3 -> encryptWithDimension3Key(input, key);
-            case TEXT -> encryptWithTextKey(input, key);
-        };
+                Random random2D = new Random(coefficient1_2D + coefficient2_2D);
+                StringBuilder gammaBuilder = new StringBuilder(gammaLength);
+                for (int i = 0; i < gammaLength; i++) {
+                    int randomNumber = random2D.nextInt(26) + 65;
+                    char randomChar = (char) randomNumber;
+                    gammaBuilder.append(randomChar);
+                }
+                return gammaBuilder.toString();
+            }
+            case DIMENSION_3 -> {
+                String[] keyParts3D = key.split(",");
+                if (keyParts3D.length != 3) {
+                    throw new IllegalArgumentException("Invalid key format for Dimension 3. " +
+                            "Expected format: coefficient1,coefficient2,coefficient3");
+                }
 
-    }
+                int coefficient1_3D;
+                int coefficient2_3D;
+                int coefficient3_3D;
+                try {
+                    coefficient1_3D = Integer.parseInt(keyParts3D[0]);
+                    coefficient2_3D = Integer.parseInt(keyParts3D[1]);
+                    coefficient3_3D = Integer.parseInt(keyParts3D[2]);
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("Invalid key format. Coefficients for Dimension 3 " +
+                            "should be integers.");
+                }
 
-    private String encryptWithDimension2Key(String input, String key) {
-        String[] keyParts = key.split(",");
-        int coefficient1 = Integer.parseInt(keyParts[0]);
-        int coefficient2 = Integer.parseInt(keyParts[1]);
+                Random random3D = new Random(coefficient1_3D + coefficient2_3D + coefficient3_3D);
+                StringBuilder gammaBuilder = new StringBuilder(gammaLength);
+                for (int i = 0; i < gammaLength; i++) {
+                    int randomNumber = random3D.nextInt(26) + 65;
+                    char randomChar = (char) randomNumber;
+                    gammaBuilder.append(randomChar);
+                }
+                return gammaBuilder.toString();
+            }
+            case NUMERIC -> {
+                try {
+                    Integer.parseInt(key);
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("Invalid key format. Numeric key should be an integer.");
+                }
 
-        StringBuilder encryptedText = new StringBuilder();
-        for (char c : input.toCharArray()) {
-            int encryptedChar = c + coefficient1 + coefficient2;
-            encryptedText.append((char) encryptedChar);
+                Random randomNumeric = new Random(key.hashCode());
+                StringBuilder gammaBuilder = new StringBuilder(gammaLength);
+                for (int i = 0; i < gammaLength; i++) {
+                    int randomNumber = randomNumeric.nextInt(10);
+                    gammaBuilder.append(randomNumber);
+                }
+                return gammaBuilder.toString();
+            }
+            case TEXT -> {
+                Random randomText = new Random(key.hashCode());
+                StringBuilder gammaBuilder = new StringBuilder(gammaLength);
+                for (int i = 0; i < gammaLength; i++) {
+                    int randomNumber = randomText.nextInt(26) + 65;
+                    char randomChar = (char) randomNumber;
+                    gammaBuilder.append(randomChar);
+                }
+                return gammaBuilder.toString();
+            }
+            default -> throw new IllegalArgumentException("Invalid key type");
         }
+    }
 
+    public String encrypt(String inputText, String gamma) {
+        StringBuilder encryptedText = new StringBuilder();
+        for (int i = 0; i < inputText.length(); i++) {
+            char inputChar = inputText.charAt(i);
+            char gammaChar = gamma.charAt(i % gamma.length());
+            char encryptedChar = (char) (inputChar ^ gammaChar);
+            encryptedText.append(encryptedChar);
+        }
         return encryptedText.toString();
     }
 
-    private String encryptWithDimension3Key(String input, String key) {
-        String[] keyParts = key.split(",");
-        int coefficient1 = Integer.parseInt(keyParts[0]);
-        int coefficient2 = Integer.parseInt(keyParts[1]);
-        int coefficient3 = Integer.parseInt(keyParts[2]);
+    public String decrypt(String encryptedText, String gamma) {
+        return encrypt(encryptedText, gamma);
+    }
 
-        StringBuilder encryptedText = new StringBuilder();
-        for (char c : input.toCharArray()) {
-            int encryptedChar = c + coefficient1 + coefficient2 + coefficient3;
-            encryptedText.append((char) encryptedChar);
+}
+
+class FileUtils {
+    public static String readFile(File file) throws IOException {
+        StringBuilder content = new StringBuilder();
+        java.util.Scanner scanner = new java.util.Scanner(file);
+        while (scanner.hasNextLine()) {
+            content.append(scanner.nextLine());
+            content.append("\n");
         }
-
-        return encryptedText.toString();
+        scanner.close();
+        return content.toString();
     }
 
-    private String encryptWithTextKey(String input, String key) {
-        StringBuilder encryptedText = new StringBuilder();
-        int keyIndex = 0;
-
-        for (char c : input.toCharArray()) {
-            int keyChar = key.charAt(keyIndex % key.length());
-            int encryptedChar = c + keyChar;
-            encryptedText.append((char) encryptedChar);
-            keyIndex++;
-        }
-
-        return encryptedText.toString();
+    public static void writeFile(File file, String content) throws IOException {
+        FileWriter writer = new FileWriter(file);
+        writer.write(content);
+        writer.close();
     }
-
-    public String decrypt(String input, String key, KeyType keyType) {
-        return switch (keyType) {
-            case DIMENSION_2 -> decryptWithDimension2Key(input, key);
-            case DIMENSION_3 -> decryptWithDimension3Key(input, key);
-            case TEXT -> decryptWithTextKey(input, key);
-        };
-
-    }
-
-    private String decryptWithDimension2Key(String input, String key) {
-        String[] keyParts = key.split(",");
-        int coefficient1 = Integer.parseInt(keyParts[0]);
-        int coefficient2 = Integer.parseInt(keyParts[1]);
-
-        StringBuilder decryptedText = new StringBuilder();
-        for (char c : input.toCharArray()) {
-            int decryptedChar = c - coefficient1 - coefficient2;
-            decryptedText.append((char) decryptedChar);
-        }
-
-        return decryptedText.toString();
-    }
-
-    private String decryptWithDimension3Key(String input, String key) {
-        String[] keyParts = key.split(",");
-        int coefficient1 = Integer.parseInt(keyParts[0]);
-        int coefficient2 = Integer.parseInt(keyParts[1]);
-        int coefficient3 = Integer.parseInt(keyParts[2]);
-
-        StringBuilder decryptedText = new StringBuilder();
-        for (char c : input.toCharArray()) {
-            int decryptedChar = c - coefficient1 - coefficient2 - coefficient3;
-            decryptedText.append((char) decryptedChar);
-        }
-
-        return decryptedText.toString();
-    }
-
-    private String decryptWithTextKey(String input, String key) {
-        StringBuilder decryptedText = new StringBuilder();
-        int keyIndex = 0;
-
-        for (char c : input.toCharArray()) {
-            int keyChar = key.charAt(keyIndex % key.length());
-            int decryptedChar = c - keyChar;
-            decryptedText.append((char) decryptedChar);
-            keyIndex++;
-        }
-
-        return decryptedText.toString();
-    }
-
 }
